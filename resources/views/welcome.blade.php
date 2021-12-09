5020d92e0b6a632cdf1843d097ca09e546d8f44b
simonmusmos <simonmusmos@gmail.com>
2021-12-10 04:06:05 +0800

    disable zoom in

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,8 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" 
+      content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
 
         <title>{{ config('app.name') }}</title>
 
